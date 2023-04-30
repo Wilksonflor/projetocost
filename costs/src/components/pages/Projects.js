03db2ad2eb62b8dac0ed266b88c0ec4a87df1a1c
Wilkson Flor <wilksonflor12@gmail.com>
2023-04-29 23:34:43 -0300

    Fazer a parte do loader

--- costs/src/components/pages/Projects.js
+++ costs/src/components/pages/Projects.js
@@ -1,6 +1,12 @@
 import {useLocation} from 'react-router-dom'
 
+
+import LinkButton from '../../layouts/LinkButton'
 import Message from "../../layouts/Message";
+import Container from '../../layouts/Container'
+
+import styles from './Project.module.css'
+
 
 function Projects(){
 
@@ -11,9 +17,15 @@ function Projects(){
         message = location.state.message
     }
     return(
-        <div>
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
             <h1>Meus projetos</h1>
+            <LinkButton to="/newproject" text="Criar Projeto" />
+            </div>
             {message && <Message type="sucess" msg={message}/>}
+            <Container customClass="start">
+                <p>Projetos...</p>
+            </Container>
         </div>
     )
 }
